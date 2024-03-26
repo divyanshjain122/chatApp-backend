4000ed2b76bb5115f71fce80dfbd57e36009da62
divyanshjain122 <divyanshjain122@gmail.com>
2024-03-26 13:22:30 +0530

    cors error resolution

--- constants/config.js
+++ constants/config.js
@@ -2,6 +2,7 @@ const corsOptions = {
   origin: [
     "http://localhost:5173",
     "http://localhost:4173",
+    "https://chatapp-frontend-two-ivory.vercel.app",
     process.env.CLIENT_URL,
   ],
   methods: ["GET", "POST", "PUT", "DELETE"],
